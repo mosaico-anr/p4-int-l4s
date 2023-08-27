3a5b0722593701df79495368a878a53d6a337afa
nhnghia <nguyenhuunghia.gtvt@gmail.com>
2023-08-27 13:34:19 +0200

    remove unvalid tests

---
+++ testbed/measure-quic/third-test/find-valid-test-having-synthetic-metrics.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# coding=utf-8
+#
+# This source code is copyrighted by Montimage. It is released under MIT license.
+# It is part of the French National Research Agency (ANR) MOSAICO project, under grant No ANR-19-CE25-0012.
+#
+# This code  print folder which contains "queue_data.csv.tar.gz"
+
+import sys, glob, re, os
+
+if len(sys.argv) != 2:
+    print("\nUsage: python3 {0} directory-of-an-experiments".format( sys.argv[0]))
+    print("   Ex: python3 {0} ./round-3/\n".format( sys.argv[0]))
+    sys.exit(1)
+
+
+DIR_PATH = sys.argv[1]
+
+REGEX = r"(?P<counter>(\d+))-bw_(?P<other>([^/]+))$"
+filenames = []
+for filename in glob.iglob(DIR_PATH + '/*', recursive=False):
+    if not os.path.isdir( filename ): 
+        continue
+
+    if not os.path.isfile( os.path.join( filename, "queue_data.csv.tar.gz")):
+        continue
+
+    # FILE_PATH = ".../unrespECN-bw_50Mbps-duration_60s--20230322-124338/data.csv"
+    match = re.search(REGEX, filename )
+
+    # if we cannot guess the traffic type and limited bandwidth
+    if not match:
+        continue
+
+    filenames.append( filename )
+
+filenames.sort()
+for f in filenames:
+    print( f )
--- testbed/measure-quic/third-test/generate-metrics-by-time-window.sh
+++ testbed/measure-quic/third-test/generate-metrics-by-time-window.sh
@@ -10,3 +10,6 @@ fi
 echo "Processing files in $dir"
 
 find "$dir/" -type d "$@" -name '*-bw_*' -print0 | xargs -0 -n1 -P16 python3 generate-metrics-by-time-window.py
+
+# compress csv file
+find "$dir/" -type f "$@" -name '*.csv' -print0 | xargs -0 -n1 -P16 -I % bash -c 'tar -czf %.tar.gz %; rm %'
